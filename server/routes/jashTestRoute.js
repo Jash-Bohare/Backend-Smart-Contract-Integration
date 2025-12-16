5e3c3007fe84b809e516d39f9c56c3265404666f
Jash-Bohare <>
2025-12-17 00:00:32 +0530

    Update test route file

---
+++ server/routes/jashTestRoute.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const jashTestRoute = require("./jashTestRoute");
+const userRoute = require("./userRoute");
+const orderRoute = require("./orderRoute");
+const paymentRoute = require("./paymentRoute");
+const productRoute = require("./productRoute");
+
+router.use("/user", userRoute);
+router.use("/order", orderRoute);
+router.use("/payment", paymentRoute);
+router.use("/product", productRoute);
+
+router.use("/", jashTestRoute);
+
+router.get("/test_ping", (req, res) => {
+  res.send("PING OK");
+
+module.exports = router;
